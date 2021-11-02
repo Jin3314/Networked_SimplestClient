37860117df84bc57de092506eac14eb2ef1acdd0
Jin3314 <dudwlstldud@gmail.com>
2021-11-02 14:20:35 -0400

    Added Login system
    Added Login system

--- Assets/GameSystemManager.cs
+++ Assets/GameSystemManager.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class GameSystemManager : MonoBehaviour
 {
 
     GameObject inputFieldUserName, inputFieldPassword, buttonSubmit, toggleLogin, toggleCreate;
+
+    GameObject networkedClient;
 
     void Start()
     {
@@ -24,6 +27,10 @@ public class GameSystemManager : MonoBehaviour
                 toggleLogin = go;
             else if (go.name == "ToggleCreate")
                 toggleCreate = go;
+            else if (go.name == "NetworkedClient")
+                networkedClient = go;
+
+          
         }
 
         buttonSubmit.GetComponent<Button>().onClick.AddListener(SubmitButtonPressed);
@@ -39,7 +46,18 @@ public class GameSystemManager : MonoBehaviour
 
     public void SubmitButtonPressed()
     {
-        Debug.Log("we good!");
+
+        string n = inputFieldUserName.GetComponent<InputField>().text;
+        string p = inputFieldPassword.GetComponent<InputField>().text;
+
+        if (toggleLogin.GetComponent<Toggle>().isOn)
+            networkedClient.GetComponent<NetworkedClient>().SendMessageToHost(ClientToServerSignifiers.Login + "," + n + "," + p);
+        else
+            networkedClient.GetComponent<NetworkedClient>().SendMessageToHost(ClientToServerSignifiers.CreateAccount + "," + n + "," + p);
+        
+
+      
+
     }
 
     public void ToggleCreateValueChanged(bool newValue)
@@ -52,4 +70,29 @@ public class GameSystemManager : MonoBehaviour
         toggleCreate.GetComponent<Toggle>().SetIsOnWithoutNotify(!newValue);
     }
 
+}
+
+public static class ClientToServerSignifiers
+{
+    public const int Login = 1;
+
+    public const int CreateAccount = 2;
+}
+
+
+public static class ServerToClientSignifiers
+{
+    public const int LoginResponse = 1;
+
+}
+
+public static class LoginResponses
+{
+    public const int Success = 1;
+
+    public const int FailureNameInUse = 2;
+
+    public const int FailureNameNotFound = 3;
+
+    public const int FailureIncorrectPassword = 4;
 }
